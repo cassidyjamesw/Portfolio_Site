6ccea68531a2fc0f8f58761aff292439ed06e49b
James Cassidy <cassidyjamesw@gmail.com>
2019-03-20 17:18:23 -0400

    images

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,29 @@
 import React from 'react'
 
 import Footer from './Footer'
-import avatar from '../assets/images/avatar.jpg'
+import avatar from '../assets/images/jamessbighead.png'
 
 class Header extends React.Component {
-    render() {
-        return (
-            <header id="header">
-                <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
-                </div>
-                <Footer />
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header id="header">
+        <div className="inner">
+          <a href="#" className="image avatar">
+            <img src={avatar} alt="" />
+          </a>
+          <h1>
+            <strong>James Cassidy </strong>
+            <div> </div>
+            Software Engineer
+            <br />
+            <div> </div>
+            Brooklyn, NY
+          </h1>
+        </div>
+        <Footer />
+      </header>
+    )
+  }
 }
 
 export default Header
