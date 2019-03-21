1d0ad1c3cc283a24cca19f6540763574f16a1dc1
James Cassidy <cassidyjamesw@gmail.com>
2019-03-21 12:58:12 -0400

    resolve error

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -8,9 +8,6 @@ import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
-import { spacing } from '@material-ui/system'
-
-import thumb01 from '../assets/images/thumbs/01.jpg'
 
 const styles = theme => ({
   card: {
