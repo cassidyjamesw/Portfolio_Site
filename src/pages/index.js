92ecb44132884231cb98e973882ad44efe6dfbb7
James Cassidy <cassidyjamesw@gmail.com>
2019-03-25 17:07:28 -0400

    removed legacy lightbox

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,49 +11,6 @@ import proj04 from '../assets/images/projectimg/goggins.png'
 import proj05 from '../assets/images/projectimg/eda.png'
 
 class HomeIndex extends React.Component {
-  constructor() {
-    super()
-
-    this.state = {
-      lightboxIsOpen: false,
-      currentImage: 0,
-    }
-
-    this.closeLightbox = this.closeLightbox.bind(this)
-    this.gotoNext = this.gotoNext.bind(this)
-    this.gotoPrevious = this.gotoPrevious.bind(this)
-    this.openLightbox = this.openLightbox.bind(this)
-    this.handleClickImage = this.handleClickImage.bind(this)
-  }
-
-  openLightbox(index, event) {
-    event.preventDefault()
-    this.setState({
-      currentImage: index,
-      lightboxIsOpen: true,
-    })
-  }
-  closeLightbox() {
-    this.setState({
-      currentImage: 0,
-      lightboxIsOpen: false,
-    })
-  }
-  gotoPrevious() {
-    this.setState({
-      currentImage: this.state.currentImage - 1,
-    })
-  }
-  gotoNext() {
-    this.setState({
-      currentImage: this.state.currentImage + 1,
-    })
-  }
-  handleClickImage() {
-    if (this.state.currentImage === this.props.images.length - 1) return
-    this.gotoNext()
-  }
-
   render() {
     const siteTitle = 'CassidyJamesW'
     const siteDescription = 'Portfolio Site'
